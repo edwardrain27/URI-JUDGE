1852f09d154902502184b0173f910702ded40c94
edwardrain27 <pablo522648@gmail.com>
2020-10-31 18:33:58 -0500

    he agregado un nuevo app

---
+++ cambiarOisicion.py
@@ -0,0 +1,17 @@
+
+
+arreglo = list()
+
+n = int(input("Digite el numero de elementos: "))
+
+for i in range(n):
+
+    arreglo.append(int(input("Digite un numero: ")))
+
+
+ultimo = arreglo[-1] #guarda la posicion del ultimo elemento del arreglo
+
+for i in range(9,1,1):
+    print(i)
+   
+
